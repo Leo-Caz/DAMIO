01d53789c0b82193894b7e9c79f0b34517386096
Leo-Caz <70967142+Leo-Caz@users.noreply.github.com>
2021-01-21 11:58:18 +0100

    added cycling of placeholder names on lcd screen

--- Programme_DAMIO.ino
+++ Programme_DAMIO.ino
@@ -5,15 +5,38 @@ int next_prgm_pin = 8;
 int start_pin = 9;
 int stop_pin = 10;
 
+char *test_prgm_names[] = {">o_/", "||_(o.o)_//", "bbbbbbbb", "uuuu"};
+int current_prgm = 0;
+int total_nb_prgm = sizeof(test_prgm_names) / sizeof(test_prgm_names[0]);
+int button_down = 0;
+int last_button_state = LOW;
+// we divide the size of the array by the size of the first ellement in order to have the number of ellement in the array.
+
 void setup() {
   pinMode (next_prgm_pin, INPUT);
   pinMode (start_pin, INPUT);
   pinMode (stop_pin, INPUT);
-  lcd.begin(16, 2);
-  lcd.print(">o_/");
+  lcd.begin (16, 2);
+  // lcd.print(">o_/");
+  lcd.setCursor(0, 0);
+  lcd.print (test_prgm_names[current_prgm]);
+  lcd.setCursor (0, 1);
+  lcd.print (test_prgm_names[(current_prgm + 1) % total_nb_prgm]);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  button_down = digitalRead(next_prgm_pin);
+  if (button_down == HIGH && last_button_state == LOW) {
+    last_button_state = HIGH;
+    current_prgm = (current_prgm + 1) % total_nb_prgm;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print (test_prgm_names[current_prgm]);
+    lcd.setCursor (0, 1);
+    lcd.print (test_prgm_names[(current_prgm + 1) % total_nb_prgm]);
+  }
 
+  else if (button_down == LOW && last_button_state == HIGH) {
+    last_button_state = LOW;
+  }
 }
